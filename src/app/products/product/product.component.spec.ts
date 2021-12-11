3a12d4a9429ef97a006ec0bd0764949fd30a0be3
michaeltaman <mictrwork@gmail.com>
2021-12-11 15:03:01 +0200

    Uses [CUSTOM_ELEMENTS_SCHEMA]

--- src/app/products/product/product.component.spec.ts
+++ src/app/products/product/product.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
@@ -8,7 +9,8 @@ describe('ProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductComponent ]
+      declarations: [ ProductComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
